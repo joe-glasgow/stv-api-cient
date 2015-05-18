485b5ba754f4d0c4df859c2b767d921bbba281cf
Joseph Morrison <josephmorrison@josephs-mbp.home>
2015-05-18 20:30:11 +0100

    local env checking

--- js/app.js
+++ js/app.js
@@ -4,5 +4,5 @@ var categories = require('./modules/player-categories');
 categories.makeApiRequest().done(function(response) {
 	console.log(response);
 }).fail(function(xhr) {
-	console.log(xhr);
+	throw new Error('API responded with status text: ' + xhr.statusText);
 });
